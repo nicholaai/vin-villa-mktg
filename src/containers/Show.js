ea5aa1c1dc543f8cc0a8b3503a7b4557a9f64cdf
Nicholas Pappagalo <npappagalo@gmail.com>
2015-08-23 20:31:36 -0700

    create email sign up

--- src/containers/Show.js
+++ src/containers/Show.js
@@ -1,10 +1,14 @@
 var React = require('react');
+var ShowEmailSignup = require('../components/ShowEmailSignup');
 
 var Show = React.createClass({
   render: function() {
     return (
       <div>
-        <h1>Show</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+          do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+          enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi</p>
+        <ShowEmailSignup />
       </div>
 
     );
