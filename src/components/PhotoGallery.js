34a919dbc1455a2454682ee2ff0f1331bbb4adae
Nicholas Pappagalo <npappagalo@gmail.com>
2015-08-24 15:45:17 -0700

    create photo gallery

---
+++ src/components/PhotoGallery.js
@@ -0,0 +1,24 @@
+var React = require('react');
+var Lightbox = require('react-lightbox');
+
+var PhotoGallery = React.createClass({
+  getInitialState: function() {
+    return {
+      pictures: [
+        'http://cdn.pitchfork.com/tracks/14505/homepage_large.c6c55cb2.jpg',
+        'http://www.billboard.com/files/styles/promo_650/public/media/drake_press-2013-650a.jpg',
+        'http://assets.vg247.com/current//2015/02/kanye-west.jpg',
+        'http://c.directlyrics.com/img/upload/lana-del-rey-big-eyes.jpg'
+      ]
+    }
+  },
+  render: function() {
+    return (
+      <div>
+        <Lightbox pictures={this.state.pictures} />
+      </div>
+    );
+  }
+});
+
+module.exports = PhotoGallery;
--- src/containers/Photo.js
+++ src/containers/Photo.js
@@ -1,12 +1,14 @@
 var React = require('react');
+var PhotoGallery = require('../components/PhotoGallery')
+import '../../stylesheets/components/PhotoGallery.css';
 
 var Photo = React.createClass({
   render: function() {
     return (
       <div>
         <h1>Photo</h1>
+        <PhotoGallery />
       </div>
-
     );
   }
 });
