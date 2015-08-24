3966db83835691a28e9f5a41d14d9581b2c67d5d
Nicholas Pappagalo <npappagalo@gmail.com>
2015-08-23 18:00:15 -0700

    embed soundcloud player

--- src/containers/Music.js
+++ src/containers/Music.js
@@ -1,10 +1,26 @@
 var React = require('react');
+var SoundCloud = require('react-soundcloud-widget');
 
 var Music = React.createClass({
-  render: function() {
+  getInitialState: function() {
+    return {
+      soundCloudUrls: [
+        'https://soundcloud.com/vin-villa/recognize',
+        'https://soundcloud.com/lnsent/what-we-do'
+      ]
+    }
+  },
+  render: function () {
+    var soundCloudPlayers = this.state.soundCloudUrls.map((url) => {
+      return(
+        <div>
+          <SoundCloud key={url} id={url} url={url} />
+        </div>
+      )
+    });
     return (
       <div>
-        <h1>Music</h1>
+        {soundCloudPlayers}
       </div>
 
     );
