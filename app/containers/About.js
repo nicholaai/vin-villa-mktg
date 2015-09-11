5dc6be52910efbb6890f5502b417c1ddf32aab15
Nicholas Pappagalo <npappagalo@gmail.com>
2015-09-10 23:44:07 -0700

    update about copy

--- app/containers/About.js
+++ app/containers/About.js
@@ -12,10 +12,11 @@ var About = React.createClass({
               born. A Los Angeles riot in his own right, Vin is a master of his craft
               who provides listeners with polished songwriting, eloquent
               vocals, and melodic production.<br/><br/>
-              Raised in <strong>Long Beach</strong>, Vin faced various obstacles as an inner
-              city youth. He witnessed the "Street Life" firsthand, as childhood
-              peers joined local gangs and sold drugs as a means of income.
-              Vin on the other hand, decided to create his own legacy.<br/><br/>
+              Raised in <strong>Long Beach</strong>, Vin faced various obstacles.
+              Growing up surrounded by the gang culture of Los Angeles exposed him
+              to the social stratification of the City of Angels. Vin has decided
+              to use his interpretations of growing up in Los Angeles as inspiration
+              for his works.<br/><br/>        
               At the age of 12, he began writing lyrics as a way of expressing
               his daily life. After years of hard work and practice, Vin built
               his home studio where his production and engineering skills began
@@ -36,14 +37,14 @@ var About = React.createClass({
               and production skills have afforded him opportunities to work
               with <strong>Juicy J, Rapper Big Pooh, Taje, and Melat</strong>. The charismatic
               youth from humble beginnings has been cosigned by well-known
-              industry producers, <strong>THX (Snoop Dogg – Don’t Stop) and Ric Rude
+              industry producers, <strong>THX (of Drop City Yacht Club) and Ric Rude
               (Destiny’s Child – Cater 2 U)</strong>. Long Beach native and legendary
               rapper <strong>Warren G</strong> has given his nod of approval when the two
               freestyled together during a chance meeting at a recording studio.
               A gifted performer, Vin has recently rocked shows at the Terrace,
               The Key Club, The M Bar, Cal State Long Beach, and USC. He has
               opened up for artists such as <strong>Casey Veggies, Raekwon,
-              and Tech N9ne's Strange Music affiliates Quest Cru</strong>.<br/><br/>
+              and Tech N9ne's Strange Music affiliates Ces Cru</strong>.<br/><br/>
               Since becoming a college grad, Vin has turned his full focus
               towards his music career with <strong>FOREVER.FREEDOM.DREAMING</strong> set to
               drop late 2015.<br/><br/>
